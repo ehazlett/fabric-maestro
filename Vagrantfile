ed4fbfbda150836b4007655fbd105b4c09c3b670
Evan Hazlett <ejhazlett@gmail.com>
2013-02-21 14:37:40 -0500

    finished mysql provisioning

---
+++ Vagrantfile
@@ -0,0 +1,17 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant::Config.run do |config|
+  config.vm.box = "precise64"
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.host_name = "maestro.local"
+  config.vm.network :hostonly, "10.10.10.30"
+  config.ssh.forward_agent = true
+  config.vm.forward_port 80, 8080 # http
+  config.vm.forward_port 10000, 10000 # services
+  config.vm.forward_port 10001, 10001 # services
+  config.vm.forward_port 10002, 10002 # services
+  config.vm.forward_port 10003, 10003 # services
+  config.vm.forward_port 10004, 10004 # services
+  config.vm.customize ["modifyvm", :id, "--memory", 1024]
+end
--- maestro/service/mysql.py
+++ maestro/service/mysql.py
@@ -1,7 +1,9 @@
 from .base import BaseService
 from fabric.api import run, task, sudo, put
 from fabric.context_managers import settings, hide
+import time
 from maestro.decorators import hosts_required
+from maestro.utils import generate_password
 import os
 
 class MySQL(BaseService):
@@ -9,6 +11,8 @@ class MySQL(BaseService):
         super(MySQL, self).__init__(*args, **kwargs)
         self._service_name = 'mysql'
         self._default_conf = '/etc/mysql/my.cnf'
+        self._default_user = 'root'
+        self._default_password = kwargs.get('default_password', None)
         self._conf = os.path.join(self._conf_dir,
             '{0}.cnf'.format(self._service_name))
         self._supervisor_conf = os.path.join(self._supervisor_conf_dir,
@@ -16,7 +20,7 @@ class MySQL(BaseService):
         self._data_dir = os.path.join(self._conf_dir, self._service_name)
         self._mysql_user = 'mysql'
 
-    def _provision(self):
+    def _provision(self, *args, **kwargs):
         # copy mysql config
         with hide('stdout', 'warnings'):
             sudo('cp -f {0} {1}'.format(self._default_conf, self._conf))
@@ -25,11 +29,12 @@ class MySQL(BaseService):
                 self._conf))
             sudo("sed -i 's/datadir.*/datadir = {0}/g' {1}".format(
                 self._data_dir.replace('/', '\/'), self._conf))
+            sudo("sed -i 's/bind-address.*/bind-address = 0.0.0.0/g' {0}".format(
+                self._conf))
             sudo("mkdir -p {0}".format(self._data_dir))
             sudo("chown -R {0} {1}".format(self._mysql_user, self._data_dir))
             sudo("mysql_install_db --defaults-extra-file={0}".format(
                 self._conf))
-            # TODO: create admin user
             # create supervisor config
             tmpl = """[program:mysql-{0}]
 command=mysqld --defaults-extra-file={1}
@@ -39,8 +44,28 @@ autorestart=true
 redirect_stderr=true
 """.format(self._name, self._conf)
             sudo("echo \"{0}\" > {1}".format(tmpl, self._supervisor_conf))
-            # refresh supervisor
-            sudo("supervisorctl update")
+
+    def _post_provision(self):
+        # wait for mysql to start
+        time.sleep(3)
+        # create admin user
+        mysql_cmd = "mysql -u {0} --port {1} -h localhost".format(
+            self._default_user, self._port)
+        if self._default_password:
+            mysql_cmd += " -p{0}".format(self._default_password)
+        password = generate_password()
+        sudo("echo \"create user 'root'@'%' identified by '{0}';\" | {1}".format(
+            password, mysql_cmd))
+        sudo("echo \"grant all privileges on *.* to 'root'@'%' with grant option\" | {0}".format(
+            mysql_cmd))
+        print("MySQL service provisioned." \
+            "  Username: root" \
+            "  Password: {0}".format(password))
+
+    def _remove(self, *args, **kwargs):
+        sudo("rm -rf {0}".format(self._conf))
+        sudo("rm -rf {0}".format(self._supervisor_conf))
+        sudo("rm -rf {0}".format(self._data_dir))
 
 @task
 @hosts_required
@@ -54,3 +79,15 @@ def create(name=None, port=None):
     """
     m = MySQL(name=name, port=port)
     m.provision()
+
+@task
+@hosts_required
+def remove(name=None):
+    """
+    Provisions a new MySQL service
+
+    :param name: Name of instance
+
+    """
+    m = MySQL(name=name)
+    m.remove()
